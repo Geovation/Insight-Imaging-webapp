99340642294605cef46a9b148787a85584e352af
JMilner <jmilner@nd29136.local>
2016-03-08 15:49:48 +0000

    Removed unecessary changes to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,6 @@
 
 var gulp = require('gulp');
 var wrench = require('wrench');
-var gls = require('gulp-live-server');
-
 /**
  *  This will load all js or coffee files in the gulp directory
  *  in order to load all gulp tasks
@@ -27,12 +25,4 @@ wrench.readdirSyncRecursive('./gulp').filter(function(file) {
  */
 gulp.task('default', ['clean'], function () {
   gulp.start('build');
-});
-
-
-// Build and serve using live reload
-gulp.task('serve', ['clean'], function(){
-    gulp.start('build');
-    var server = gls.static('dist'); //equals to gls.static('public', 3000);
-    server.start();
 });
