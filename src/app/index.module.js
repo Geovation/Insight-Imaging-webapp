4c405afc39e4f243c1f6c4b3b070f9b1f043505b
JMilner <jmilner@nd29136.local>
2016-04-05 14:19:39 +0100

    Use  instead of native promises (IE Support)

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,8 @@
   'use strict';
 
   angular
-    .module('insight-imaging-webapp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr','firebase']);
+    .module('insight-imaging-webapp',
+      ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr', 'firebase']
+    );
 
 })();
