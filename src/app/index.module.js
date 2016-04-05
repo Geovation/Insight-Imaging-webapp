9a43598e20837a776a11b02b562b71f1e9afbfc9
JMilner <jmilner@nd29136.local>
2016-04-05 19:15:45 +0100

    Tried to make more responsive, updated Angular Material

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -3,7 +3,7 @@
 
   angular
     .module('insight-imaging-webapp',
-      ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr', 'firebase']
+      ['ngAnimate', 'ngCookies', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr', 'firebase']
     );
 
 })();
